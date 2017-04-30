77759493ea42499ee0a86b4d2c0c06c00f5f372c
tiayo <474993693@qq.com>
2017-04-30 23:13:52 +0800

    error

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Console;
 
-use App\Service\TaskCheckService;
+use App\Console\Commands\TaskCheck;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\TaskCheck::class
+        TaskCheck::class
     ];
 
     /**
