697a87b91dfeb4930fdbf4560dd267c82ed672a8
zxj <656861622@qq.com>
2017-05-17 11:26:15 +0800

    添加微信回调接口路由

--- routes/payment.php
+++ routes/payment.php
@@ -8,7 +8,6 @@ Route::group(['middleware' => 'auth', 'namespace' => 'Admin', 'prefix' => 'admin
     Route::get('/alipay/query/{order}', 'AlipayController@query');
     Route::post('/alipay/pay', 'AlipayController@pay');
     Route::get('/alipay/callback', 'AlipayController@callback');
-    Route::post('/alipay/app', 'AlipayController@app');
     Route::get('/alipay/refund/{order_id}', 'AlipayController@refundView');
     Route::post('/alipay/refund/{order_id}', 'AlipayController@refundAction');
 
@@ -16,5 +15,10 @@ Route::group(['middleware' => 'auth', 'namespace' => 'Admin', 'prefix' => 'admin
     Route::get('/weixin/refund/{order_id}', 'WeixinController@refundView');
     Route::post('/weixin/refund/{order_id}', 'WeixinController@refundAction');
     Route::post('/weixin/pay', 'WeixinController@pay');
+
+});
+
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+    Route::post('/alipay/app', 'AlipayController@app');
     Route::post('/weixin/app', 'WeixinController@app');
 });
