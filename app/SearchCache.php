fc9602a36d94efc9ef8270c3364241c664e04843
tiayo <474993693@qq.com>
2017-06-05 19:20:30 +0800

    添加文章页面搜索功能

--- app/SearchCache.php
+++ app/SearchCache.php
@@ -9,8 +9,9 @@ class SearchCache extends Model
     public $timestamps = true;
     protected $fillable = [
         'content',
+        'relevance',
     ];
-    protected $connection = 'mysql2';
+    protected $connection = 'mysql';
     protected $table = 'search_cache';
     protected $primaryKey = 'search_id';
 
--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             'engine' => null,
         ],
 
-        'mysql2' => [
+        'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
