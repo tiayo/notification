d18378c6c9532481f821e141da6a3a32e0f347da
zxj <656861622@qq.com>
2017-06-13 14:38:19 +0800

    修复消息列表页面js逻辑问题

--- resources/views/home/message_list.blade.php
+++ resources/views/home/message_list.blade.php
@@ -108,8 +108,10 @@
                 <h5 class="text-center" id="message-float-send-user"></h5>
                 <p id="message-float-content"></p>
                 <p class="text-center">
+                    @if ($row['user_id'] != Auth::id())
                     <button class="btn btn-wide btn-loading btn-primary" id="message-float-reply">回复</button>
                     <button class="btn btn-wide btn-loading btn-primary" id="message-float-no">设为未读状态</button>
+                    @endif
                     <button class="btn btn-wide btn-loading btn-primary" id="message-float-close">关闭</button>
                 </p>
             </div>
@@ -122,61 +124,5 @@
     <script src="/javascripts/template-init.min.js"></script>
     <script src="/vendor/data-table/media/js/jquery.dataTables.min.js"></script>
     <script src="/vendor/data-table/media/js/dataTables.bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function () {
-
-            //消息框显示
-            $('tbody tr').click(function () {
-                message_id_parent = $(this).attr('id');
-                var message_id_num = $(this).attr('num');
-                $('.message-bgc').removeClass('hidden');
-                $('.message-float').removeClass('hidden');
-                $('#message-float-content').html($('#message-id-content'+message_id_num).html());
-                $('#message-float-send-user').html('来自:'+$('#message-id-send-user').html());
-
-                //设置文章为已读
-                already_read();
-            });
-
-            //回复按钮
-            $('#message-float-reply').click(function () {
-                window.location.href = $('#message-id-reply').attr('href');
-            });
-
-            //设置文章未读
-            $('#message-float-no').click(function () {
-                $('.message-bgc').addClass('hidden');
-                $('.message-float').addClass('hidden');
-                no_read();
-            });
-            
-            //消息框关闭1
-            $('.message-bgc').click(function () {
-                $('.message-bgc').addClass('hidden');
-                $('.message-float').addClass('hidden');
-                $('#'+message_id_parent).removeClass('color-danger');
-                $('#message-id-status').html('已读');
-            });
-            //消息框关闭2
-            $('#message-float-close').click(function () {
-                $('.message-bgc').addClass('hidden');
-                $('.message-float').addClass('hidden');
-                $('#'+message_id_parent).removeClass('color-danger');
-                $('#message-id-status').html('已读');
-            });
-
-            //设置文章为已读
-            function already_read() {
-                axios.get($('#message-id-statusurl').attr('url')+'2');
-            }
-
-            //设置文章为未读
-            function no_read() {
-                $('#'+message_id_parent).addClass('color-danger');
-                $('#message-id-status').html('未读');
-
-                axios.get($('#message-id-statusurl').attr('url')+'1')
-            }
-        })
-    </script>
+    @include('home.message_js')
 @endsection
